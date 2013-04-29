8aae1f381cc4a50b00b7a9296a2fd4f8db8dbc46
Lars Haugseth <lars@larshaugseth.com>
2013-04-29 09:26:03 +0200

    Added setting of locale.

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -1,6 +1,7 @@
 # Required Ubuntu packages
 package "curl"
 package "libcurl4-openssl-dev"
+package "locales"
 
 # Useful Ubuntu packages
 package "screen"
--- recipes/system.rb
+++ recipes/system.rb
@@ -2,3 +2,10 @@
 execute "fix_not_a_tty_warning" do
   command "sed -ie 's/^mesg n$/if `tty -s`; then\\n  mesg n\\nfi/' /root/.profile"
 end
+
+# Configure proper locale settings
+language = 'en_US.UTF8'
+execute "Set system locale" do
+  command "update-locale LANG=#{language}"
+  not_if "cat /etc/default/locale | grep -qx LANG=#{language}"
+end
