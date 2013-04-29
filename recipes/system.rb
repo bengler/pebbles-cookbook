cff2bb80a2e5341f8300e5e9b4888303c5e23a42
Lars Haugseth <github@larshaugseth.com>
2013-04-29 10:36:58 +0200

    Added installation of locale if missing.

--- recipes/system.rb
+++ recipes/system.rb
@@ -1,11 +1,17 @@
 # Fix cause of 'stdin: not a tty' warnings from Chef
-execute "fix_not_a_tty_warning" do
-  command "sed -ie 's/^mesg n$/if `tty -s`; then\\n  mesg n\\nfi/' /root/.profile"
+execute "Fix not-a-tty warning" do
+  command %(sed -ie 's/^mesg n$/if `tty -s`; then\\n  mesg n\\nfi/' /root/.profile)
 end
 
-# Configure proper locale settings
-language = 'en_US.UTF8'
+# Configure locale
+language = 'nb_NO.utf8'
+
+execute "Add locale" do
+  command %(locale-gen #{language})
+  not_if  %(locale -a | grep -qix #{language})
+end
+
 execute "Set system locale" do
-  command "update-locale LANG=#{language}"
-  not_if "cat /etc/default/locale | grep -qx LANG=#{language}"
+  command %(update-locale LANG=#{language})
+  not_if  %(cat /etc/default/locale | grep -qix LANG=#{language})
 end
