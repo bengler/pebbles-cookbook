e3f85d28d69b3ab4d6e2d94ee4dc5c6eca9c41ff
Lars Haugseth <lars@larshaugseth.com>
2013-05-08 15:05:55 +0200

    Added memcached.

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -1,7 +1,8 @@
-# Required Ubuntu packages
+# Required Ubuntu packages not installed by default
 package "curl"
 package "libcurl4-openssl-dev"
 package "locales"
+package "memcached"
 
 # Useful Ubuntu packages
 package "screen"
