480a16f9de787d15f3f4e48724725b50617ebf7c
Lars Haugseth <github@larshaugseth.com>
2013-10-10 15:18:08 +0200

    Removed ugly "apt-get update" hack, use newer version of apt recipie instead.

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -1,14 +1,3 @@
-# Update package repository list initially and again if
-# more than 24h has passed since last time
-execute "apt-get-update-periodic" do
-  command "apt-get update"
-  ignore_failure true
-  only_if do
-    !(File.exists?('/var/lib/apt/periodic/update-success-stamp')) ||
-      (File.mtime('/var/lib/apt/periodic/update-success-stamp') < Time.now - 86400)
-  end
-end
-
 # Required Ubuntu packages not installed by default
 package "curl"
 package "libcurl4-openssl-dev"
