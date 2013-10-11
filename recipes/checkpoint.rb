6625d2fdf129812db2d3ee4a172a71ffabf3d783
Lars Haugseth <github@larshaugseth.com>
2013-10-11 10:23:25 +0200

    Added creation of amedia realm for checkpoint.

--- recipes/checkpoint.rb
+++ recipes/checkpoint.rb
@@ -7,6 +7,8 @@ bundle_install
 rake_task "db:migrate"
 rake_task "db:migrate", :env => :test
 
+bundle_exec './bin/checkpoint realm create amedia -t \'Amedia Security Realm\' -d amedia.no'
+
 ensure_log_directory
 
 brow_symlink
