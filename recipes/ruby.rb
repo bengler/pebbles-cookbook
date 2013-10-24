92b780fd7f84bbda615db84b1e64677927feddce
Lars Haugseth <github@larshaugseth.com>
2013-10-24 14:26:51 +0200

    Removed Ruby 2.0 dependency.

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -3,7 +3,7 @@
 node.set['rvm']['user_installs'] = [
   { 'user'         => 'vagrant',
     'default_ruby' => '1.9.3',
-    'rubies'       => ['1.9.3', '2.0.0']
+    'rubies'       => ['1.9.3']
   }
 ]
 
