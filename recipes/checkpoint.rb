94010c52cd0d8c5e9c43c0cc2803756dc60200b5
Lars Haugseth <github@larshaugseth.com>
2013-10-24 14:26:34 +0200

    Removed checkpoint realm creation.

--- recipes/checkpoint.rb
+++ recipes/checkpoint.rb
@@ -7,8 +7,6 @@ bundle_install
 rake_task "db:migrate"
 rake_task "db:migrate", :env => :test
 
-bundle_exec './bin/checkpoint realm create amedia -t \'Amedia Security Realm\' -d amedia.no'
-
 ensure_log_directory
 
 brow_symlink
